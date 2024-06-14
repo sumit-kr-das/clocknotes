8235e46d52a9d47013cf8ec0a23e1e0a715c2109
sumit-kr-das <sumitkrdas@gmail.com>
2024-06-14 11:16:26 +0530

    raw data updated

--- src/app/(main)/ws/[workspaceId]/calender/__components/customData.ts
+++ src/app/(main)/ws/[workspaceId]/calender/__components/customData.ts
@@ -4,39 +4,29 @@ export const EVENTS = [
   {
     start: dayjs("2024-06-10T10:00:00").toDate(),
     end: dayjs("2024-06-10T11:00:00").toDate(),
-    data: {
-      appointment: {
-        id: 1,
-        status: "P",
-        location: "New York",
-        resource: "Dr Alex",
-        address: "Building 5\nStreet 44\nNear Express Highway\nNew York",
-      },
-    },
+    name: "Fixing navigation bar",
+    isBillable: true,
+    project: "Company 1",
+    tags: ["node", "reactjs", "mariaDB"],
+    color: "",
     isDraggable: true,
   },
   {
     start: dayjs("2024-06-10T12:00:00").toDate(),
     end: dayjs("2024-06-10T13:00:00").toDate(),
-    data: {
-      appointment: {
-        id: 2,
-        status: "CI",
-        location: "Washington",
-        resource: "Dr David",
-        address: "Block 1\nSStreet 32\nLong Island\nNew York",
-      },
-    },
-    isDraggable: false,
+    name: "Home page complete",
+    isBillable: true,
+    project: "Company 1",
+    tags: ["node", "reactjs", "mariaDB"],
+    isDraggable: true,
   },
   {
     start: dayjs("2024-06-12T09:00:00").toDate(),
     end: dayjs("2024-06-12T14:59:59").toDate(),
-    data: {
-      blockout: {
-        id: 1,
-        name: "Christmas Holidays",
-      },
-    },
+    name: "Fixing navigation bar",
+    isBillable: false,
+    project: "NPTEL",
+    tags: ["node", "reactjs", "mariaDB"],
+    isDraggable: true,
   },
 ];
